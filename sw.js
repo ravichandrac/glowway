9b8aaa5efcc6bd3bde9a6142a6b87a8732a72f73
ravichandrac <87170529+ravichandrac@users.noreply.github.com>
2026-07-28 00:05:29 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "glowway-v13";
-const CORE_ASSETS = ["./", "./index.html", "./styles.css?v=13", "./app.js?v=13", "./manifest.json", "./assets/birmingham-droitwich-art.png"];
+const CACHE_NAME = "glowway-v14";
+const CORE_ASSETS = ["./", "./index.html", "./styles.css?v=14", "./app.js?v=14", "./manifest.json", "./assets/birmingham-droitwich-art.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))));
 self.addEventListener("activate", (event) => {
   event.waitUntil(
